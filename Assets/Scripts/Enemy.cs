d34d96d632123842195b3463241018143622b76c
Bishop Bader <bflatbader@gmail.com>
2023-11-16 14:21:12 -0500

    Feature/player (#1)
    * init project
    
    * Added player movement
    
    * Added player bounds
    
    * Movement code into its own class, added ref to Update
    
    * Added laser firing
    
    * Enemy movement behavior
    
    * Add collision events
    
    * Added tags
    
    * Player damage and death
    
    * forgot to remove TODO
    
    * removed another TODO

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 4.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        
+        // Respawn at top with new random x position after off-screen
+        if (transform.position.y < -5.5f)
+        {
+            float randomX = Random.Range(-8.0f, 8.0f);
+            transform.position = new Vector3(randomX, 8.0f, 0);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            // Damage the player and destroy the enemy
+            other.transform.GetComponent<Player>().Damage();
+            Destroy(this.gameObject);
+        }
+        else if (other.tag == "Laser")
+        {
+            // Destroy the laser and the enemy
+            Destroy(other.transform.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 3.5f;
+    [SerializeField]
+    private int _lives = 3;
+    [SerializeField]
+    private GameObject _laserPrefab;
+    [SerializeField]
+    private float _fireRate = 0.5f;
+    private float _fireCooldown = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _fireCooldown)
+        {
+            FireLaser();
+        }
+    }
+
+    void CalculateMovement()
+    {
+        // Move player
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+        // Player Y-axis boundaries
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
+
+        // Player position wrapping on X-axis
+        if (transform.position.x >= 11.3f)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -11.3f)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
+    }
+
+    void FireLaser()
+    {
+        _fireCooldown = Time.time + _fireRate;
+
+        Vector3 laserSpawnPosition = new Vector3(transform.position.x, transform.position.y + 0.8f, 0);
+        Instantiate(_laserPrefab, laserSpawnPosition, Quaternion.identity);
+    }
+
+    public void Damage()
+    {
+        _lives -= 1;
+
+        if (_lives < 1)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
